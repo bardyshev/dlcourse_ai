edfda54b34e8d6aa5124afed48a2e879a7a418b8
abardyshev <abardyshev@cian.ru>
2019-04-15 19:46:25 +0300

    check gradient realization

--- assignments/assignment1/gradient_check.py
+++ assignments/assignment1/gradient_check.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def check_gradient(f, x, delta=1e-5, tol = 1e-4):
+def check_gradient(f, x, delta=1e-5, tol=1e-4):
     '''
     Checks the implementation of analytical gradient by comparing
     it to numerical gradient using two-point formula
@@ -32,9 +32,15 @@ def check_gradient(f, x, delta=1e-5, tol = 1e-4):
     while not it.finished:
         ix = it.multi_index
         analytic_grad_at_ix = analytic_grad[ix]
-        numeric_grad_at_ix = 0
 
-        # TODO compute value of numeric gradient of f to idx
+        x_plus = x.copy()
+        x_plus[ix] = x_plus[ix] + delta
+
+        x_minus = x.copy()
+        x_minus[ix] = x_minus[ix] - delta
+
+        numeric_grad_at_ix = (f(x_plus)[0] - f(x_minus)[0]) / (2 * delta)
+
         if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
             print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (ix, analytic_grad_at_ix, numeric_grad_at_ix))
             return False
@@ -43,7 +49,3 @@ def check_gradient(f, x, delta=1e-5, tol = 1e-4):
 
     print("Gradient check passed!")
     return True
-
-        
-
-        
